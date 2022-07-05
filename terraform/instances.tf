800976878cd4b785aed659bd4796ebc2faad939b
axelallain <axel.allain.a@gmail.com>
2022-07-05 13:16:14 +0000

    install-postgres.yml ansible playbook and terraform for the database.

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,5 +1,15 @@
 resource "linode_instance" "nanode-1" {
 	image = "linode/centos7"
+	label = "postgres-db"
+	group = "database"
+	region = var.region
+	type = "g6-nanode-1"
+	# authorized_keys = { var.authorized_keys }
+	root_pass = var.root_pass
+}
+
+resource "linode_instance" "nanode-2" {
+	image = "linode/centos7"
 	label = "api-postable-app"
 	group = "centos"
 	region = var.region
